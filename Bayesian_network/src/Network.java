dc5adc1592c46a0f18b94bb60197c2ee4c169a9f
YairIvgi <yair5700@gmail.com>
2018-12-23 01:59:49 +0200

    update and adaptation for the second algorithm

--- Bayesian_network/src/Network.java
+++ Bayesian_network/src/Network.java
@@ -16,143 +16,143 @@ import java.util.Map;
  */
 public class Network {
 
-	private  Map<String,Node> nodesMap;
-	private  List<Node> nodes;
-	private  List<Query>  queries;
-	private List<Result> results;
+    private  Map<String,Node> nodesMap;
+    private  List<Node> nodes;
+    private  List<Query>  queries;
+    private List<Result> results;
 
-	public Network(){
-		nodes = new ArrayList<Node>();
-		queries = new ArrayList<Query>();
-		nodesMap = new HashMap<String,Node>();
-		results = new ArrayList<Result>();
-	}
+    public Network(){
+	nodes = new ArrayList<Node>();
+	queries = new ArrayList<Query>();
+	nodesMap = new HashMap<String,Node>();
+	results = new ArrayList<Result>();
+    }
 
-	/**
-	 * this method reads the network from a designated file and phrase it to the data structure 
-	 * @author Yair Ivgi
-	 * @return void
-	 * @throws Exception
-	 */
-	public  void readFile(String fileName) throws Exception{
-		File file  =new File(fileName);
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			String line = br.readLine();
-			if(!line.startsWith("Network")){
-				br.close();
-				throw new Exception("Text is not in the correct format\n");
-			}
-			line = br.readLine();
-			line=line.replace("Variables: ", "");
-			String tempVar[] = line.split(",");
-			for (int i = 0; i < tempVar.length; i++) {
-				String name = tempVar[i];
-				Node node =new Node(name);
-				nodesMap.put(name, node);
-				nodes.add(node);
-			}
-			while(!line.startsWith("Queries")){
-				line=br.readLine();
-				if(line.startsWith("Var ")){
-					String name=line.replace("Var ", "");
-					Node node = nodesMap.get(name); 
-					if(node == null){
-						br.close();
-						throw new Exception("Text is not in the correct format\n");
-					}
-					line=br.readLine();
-					node.addValues(line);
-					line=br.readLine();
-					if(line.startsWith("Parents")){
-						String par =  line.replace("Parents: ", "");
-						if(!par.startsWith("none")){
-							String[] tempParent = par.split(",");
-							for(String t:tempParent){
-								Node n =nodesMap.get(t);
-								if(n == null){
-									br.close();
-									throw new Exception("The parent is not in the Variables list\n");
-								}
-								node.addParent(n);	
-							}
-						}
-					}
-					line=br.readLine();
-					if(line.startsWith("CPT")){
-						line =br.readLine();
-						while(!line.isEmpty()){
-							node.addCPT(line);
-							line =br.readLine();
-						}
-					}
-				}
-			}
-			if(line.startsWith("Queries")){
-				line=br.readLine();
-				while(line!= null){
-					Query query =new Query();
-					query.addValue(line, nodesMap);
-					queries.add(query);
-					line=br.readLine();
-				}
-			}
+    /**
+     * this method reads the network from a designated file and phrase it to the data structure 
+     * @author Yair Ivgi
+     * @return void
+     * @throws Exception
+     */
+    public  void readFile(String fileName) throws Exception{
+	File file  =new File(fileName);
+	try {
+	    BufferedReader br = new BufferedReader(new FileReader(file));
+	    String line = br.readLine();
+	    if(!line.startsWith("Network")){
+		br.close();
+		throw new Exception("Text is not in the correct format\n");
+	    }
+	    line = br.readLine();
+	    line=line.replace("Variables: ", "");
+	    String tempVar[] = line.split(",");
+	    for (int i = 0; i < tempVar.length; i++) {
+		String name = tempVar[i];
+		Node node =new Node(name);
+		nodesMap.put(name, node);
+		nodes.add(node);
+	    }
+	    while(!line.startsWith("Queries")){
+		line=br.readLine();
+		if(line.startsWith("Var ")){
+		    String name=line.replace("Var ", "");
+		    Node node = nodesMap.get(name); 
+		    if(node == null){
 			br.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-		}
-
-	}
-
-	/**
-	 * this method handles the different algorithm requirement from the file
-	 * @author Yair Ivgi
-	 * @return void
-	 * @throws Exception
-	 */
-	public void executeQueries() throws Exception{
-		Algorithms a =new Algorithms(nodes);	
-		for (int i = 0; i < queries.size(); i++) {
-			switch (queries.get(i).getAlgoType()) {
-			case 1:
-				//		results.add(a.calculate1(queries.get(i)));	
-				continue;
-			case 2:
-				results.add(a.calculate2(queries.get(i)));
-				continue;
-			case 3:
-				results.add(a.calculate3(queries.get(i)));
-				continue;
-			default:
-				continue;
+			throw new Exception("Text is not in the correct format\n");
+		    }
+		    line=br.readLine();
+		    node.addValues(line);
+		    line=br.readLine();
+		    if(line.startsWith("Parents")){
+			String par =  line.replace("Parents: ", "");
+			if(!par.startsWith("none")){
+			    String[] tempParent = par.split(",");
+			    for(String t:tempParent){
+				Node n =nodesMap.get(t);
+				if(n == null){
+				    br.close();
+				    throw new Exception("The parent is not in the Variables list\n");
+				}
+				node.addParent(n);	
+			    }
 			}
+		    }
+		    line=br.readLine();
+		    if(line.startsWith("CPT")){
+			line =br.readLine();
+			while(!line.isEmpty()){
+			    node.addCPT(line);
+			    line =br.readLine();
+			}
+		    }
 		}
+	    }
+	    if(line.startsWith("Queries")){
+		line=br.readLine();
+		while(line!= null){
+		    Query query =new Query();
+		    query.addValue(line, nodesMap);
+		    queries.add(query);
+		    line=br.readLine();
+		}
+	    }
+	    br.close();
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
 	}
 
-	/**
-	 * this method writs the results to the output file
-	 * @author Yair Ivgi
-	 * @return void
-	 * @throws IOException
-	 */
-	public void writeFile() throws IOException{
-		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-		for(Result res : results){
-			writer.write(res.toString());
-			writer.newLine();
-		}
-		writer.close();
-	}
+    }
 
-	public  void print(){
-		for (int i = 0; i < nodes.size(); i++) {
-			nodes.get(i).print();
-			System.out.println();
-		}
-		System.out.println("Queries: ");
-		for (int i = 0; i < queries.size(); i++) {
-			queries.get(i).print();
-			System.out.println();
-		}
+    /**
+     * this method handles the different algorithm requirement from the file
+     * @author Yair Ivgi
+     * @return void
+     * @throws Exception
+     */
+    public void executeQueries() throws Exception{
+	Algorithms a =new Algorithms(nodes);	
+	for (int i = 0; i < queries.size(); i++) {
+	    switch (queries.get(i).getAlgoType()) {
+	    case 1:
+		//	results.add(a.calculate1(queries.get(i)));	
+		continue;
+	    case 2:
+		results.add(a.calculate2(queries.get(i)));
+		continue;
+	    case 3:
+		//	results.add(a.calculate3(queries.get(i)));
+		continue;
+	    default:
+		continue;
+	    }
 	}
+    }
+
+    /**
+     * this method writs the results to the output file
+     * @author Yair Ivgi
+     * @return void
+     * @throws IOException
+     */
+    public void writeFile() throws IOException{
+	BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+	for(Result res : results){
+	    writer.write(res.toString());
+	    writer.newLine();
+	}
+	writer.close();
+    }
+
+//    public  void print(){
+//	for (int i = 0; i < nodes.size(); i++) {
+//	    nodes.get(i).print();
+//	    System.out.println();
+//	}
+//	System.out.println("Queries: ");
+//	for (int i = 0; i < queries.size(); i++) {
+//	    queries.get(i).print();
+//	    System.out.println();
+//	}
+//    }
 }
--- Bayesian_network/src/Node.java
+++ Bayesian_network/src/Node.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -8,152 +7,139 @@ import java.util.List;
  */
 
 public class Node{
-	private String name;
-	private List<Node> parents;	
-	private List<String> values;
-	private List<CPT> cpt;
-	private List<String> joinedNodes;
+    private String name;
+    private List<Node> parents;	
+    private List<String> values;
+    private List<CPT> cpt;
+    private boolean isHidden;
 
-	public Node(String name){
-		this.name=name;
-		parents =new ArrayList<Node>();
-		values =new ArrayList<String>();
-		cpt =new ArrayList<CPT>();
-		joinedNodes = new ArrayList<String>();
+    public void setHidden(boolean isHidden) {
+	this.isHidden = isHidden;
+    }
+
+    public boolean isHidden() {
+	return isHidden;
+    }
+
+    public Node(String name){
+	this.name=name;
+	parents =new ArrayList<Node>();
+	values =new ArrayList<String>();
+	cpt =new ArrayList<CPT>();
+    }
+
+    public List<Node> getParents(){ return parents;} 
+
+    public List<String> getValues(){ return values;}
+
+    public List<CPT> getCPT(){ return cpt;}
+
+    public String getName(){ return name;}
+
+    public void addValues(String line){
+	String val = line.replace("Values: ", "");
+	String[] tempVals =val.split(",");
+	for (int i = 0; i < tempVals.length; i++) {
+	    values.add(tempVals[i].trim());
 	}
+    }
 
-	public void addJoinedNode(String name){
-		joinedNodes.add(name);
+    public void addParent(Node node){
+	parents.add(node);
+    }
+
+    public void addCPT(String line){
+	CPT c = new CPT(values,parents);
+	c.addCPT(line);
+	cpt.add(c);
+    }
+
+//    public void print(){
+//	System.out.println(name);
+//	System.out.print("Values: ");
+//	for(String s : values){
+//	    System.out.print(s+",");
+//	}
+//	System.out.println();
+//	System.out.print("Parents: ");
+//	for(Node s : parents){
+//	    System.out.print(s.getName()+",");
+//	}
+//	System.out.println();
+//	System.out.println("CPT: ");
+//	for(CPT c : cpt){
+//	    c.printCpt();
+//	    System.out.println("\n");
+//	}
+//    }
+    
+    public void print2(){
+	System.out.println(name);
+    }
+
+
+    /**
+     * This method checks if two Nodes are equal
+     * @author Yair Ivgi
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+	if (!(o instanceof Node)){
+	    return false;
 	}
-
-	public List<String> getJoinedNodes() {
-		return joinedNodes;
+	Node n =(Node) o;
+	if(n.getName().equals(getName())){
+	    return true;
 	}
+	return false;
+    }
 
-	public List<Node> getParents(){ return parents;} 
-
-	public List<String> getValues(){ return values;}
-
-	public List<CPT> getCPT(){ return cpt;}
-
-	public String getName(){ return name;}
-
-	public void addValues(String line){
-		String val = line.replace("Values: ", "");
-		String[] tempVals =val.split(",");
-		for (int i = 0; i < tempVals.length; i++) {
-			values.add(tempVals[i].trim());
-		}
+    /**
+     * This method removes the non relevant nodes to the specific query from the bayesian network   
+     * @author Yair Ivgi
+     * @return List<Node>
+     */
+    static List<Node> removeNotRelaventNodes(List<Vnode> query) {
+	List<Node> qList = new ArrayList<Node>();
+	List<Node> parents = new ArrayList<Node>();
+	for(Vnode vn : query){
+	    qList.add(vn.getNode());
 	}
-
-	public void addParent(Node node){
-		parents.add(node);
+	for(Node n : qList){
+	    List<Node> nodes = new ArrayList<Node>();
+	    parents.addAll(n.recursive(nodes));
 	}
+	qList.addAll(parents);
+	return getDistinctList(qList);
+    }
 
-	public void addCPT(String line){
-		CPT c = new CPT(values,parents);
-		c.addCPT(line);
-		cpt.add(c);
+    /**
+     * This method recurse on the ancestors of a specific node and returns them   
+     * @author Yair Ivgi
+     * @return List<Node>
+     */
+    private List<Node> recursive(List<Node> nodes){
+	if(! (this.getParents().size()==0)){
+	    for(Node n : this.getParents()){
+		nodes.add(n);
+		n.recursive(nodes);
+	    }
 	}
+	return nodes;
+    }
 
-	public void print(){
-		System.out.println(name);
-		System.out.print("Values: ");
-		for(String s : values){
-			System.out.print(s+",");
-		}
-		System.out.println();
-		System.out.print("Parents: ");
-		for(Node s : parents){
-			System.out.print(s.getName()+",");
-		}
-		System.out.println();
-		System.out.println("CPT: ");
-		for(CPT c : cpt){
-			c.printCpt();
-			System.out.println("\n");
-		}
+    /**
+     * This method returns a distinct list from a list of nodes    
+     * @author Yair Ivgi
+     * @return List<Node>
+     */
+    private static List<Node> getDistinctList(List<Node> list){
+	List<Node> listDistinct = new ArrayList<>();
+	for(Node i : list){
+	    if( !listDistinct.contains(i)){
+		listDistinct.add(i); }
 	}
-
-	/**
-	 * This method checks if two Nodes are equal
-	 * @author Yair Ivgi
-	 * @return boolean
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof Node)){
-			return false;
-		}
-		Node n =(Node) o;
-		if(n.getName().equals(getName())){
-			return true;
-		}
-		return false;
-	}
-
-	void eliminate() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * This method removes the non relevant nodes to the specific query from the bayesian network   
-	 * @author Yair Ivgi
-	 * @return List<Node>
-	 */
-	public static List<Node> removeNotRelaventNodes(List<Vnode> query) {
-		List<Node> qList = new ArrayList<Node>();
-		List<Node> parents = new ArrayList<Node>();
-		for(Vnode vn : query){
-			qList.add(vn.getNode());
-		}
-		for(Node n : qList){
-			List<Node> nodes = new ArrayList<Node>();
-			parents.addAll(n.recursive(nodes));
-		}
-		qList.addAll(parents);
-		return getDistinctList(qList);
-	}
-
-	/**
-	 * This method recurse on the ancestors of a specific node and returns them   
-	 * @author Yair Ivgi
-	 * @return List<Node>
-	 */
-	public List<Node> recursive(List<Node> nodes){
-		if(! (this.getParents().size()==0)){
-			for(Node n : this.getParents()){
-				nodes.add(n);
-				n.recursive(nodes);
-			}
-		}
-		return nodes;
-	}
-	
-	/**
-	 * This method returns a distinct list from a list of nodes    
-	 * @author Yair Ivgi
-	 * @return List<Node>
-	 */
-	private static List<Node> getDistinctList(List<Node> list){
-		List<Node> listDistinct = new ArrayList<>();
-		for(Node i : list){
-			if( !listDistinct.contains(i)){
-				listDistinct.add(i); }
-		}
-		return listDistinct;
-	}
-}
-
-/**
- *class that implements comparator and compare alphabetically list of nodes
- *@author Yair Ivgi
- */
-
-class alphabeticalComparator implements Comparator<Node>{
-	@Override
-	public int compare(Node o1, Node o2) {
-		return o1.getName().compareTo(o2.getName());
-	}
+	return listDistinct;
+    }
 }
